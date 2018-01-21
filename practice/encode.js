1aa2fbb27f942b7aeb9435c8aa9d9639d4736e60
ouyangxuanyun <youtianzhan@126.com>
2018-01-21 21:37:52 +0800

    delete promise

---
+++ practice/encode.js
@@ -0,0 +1,10 @@
+let promise = new Promise(function(resolve,rejeact){
+    console.log('Promise');
+    resolve();
+});
+
+// promise.then(function(){
+//     console.log('Resolved');
+// });
+
+console.log('Hi');
